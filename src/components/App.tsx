4753feb61f8dfeb216be033d78dcc2933a7f258f
eduter <eduter@fareoffice.com>
2020-04-23 14:25:04 +0200

    Added some styling to collection page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,14 @@ import { Route, Switch } from 'react-router';
 import MovieCollectionPage from './pages/MovieCollectionPage';
 import MovieDetailsPage from './pages/MovieDetailsPage';
 import NotFoundPage from './pages/NotFoundPage';
+import { createGlobalStyle } from 'styled-components';
 
 
 function App() {
   return (
     <div>
+      <GlobalStyle/>
+      <h1>Chris' Movie Collection</h1>
       <Switch>
         <Route exact path="/" component={MovieCollectionPage}/>
         <Route exact path="/movieDetails/:id" component={MovieDetailsPage}/>
@@ -16,6 +19,14 @@ function App() {
     </div>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Roboto, sans-serif;
+    background-color: #000;
+    color: gray;
+  }
+`;
 
 
 export default App;
--- src/components/pages/MovieCollectionPage.tsx
+++ src/components/pages/MovieCollectionPage.tsx
@@ -1,32 +1,78 @@
+import { push } from 'connected-react-router';
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
 import { Dispatch } from 'redux';
+import styled from 'styled-components';
 import { State } from '../../store/types';
 import { movies } from '../../movies';
-import { push } from 'connected-react-router';
 
 
 interface MovieCollectionPageProps extends ConnectedProps<typeof connector> {
 }
 
-
 function MovieCollectionPage({ movies, goToDetails }: MovieCollectionPageProps) {
   return (
-    <div>
-      <h1>Chris' Movie Collection</h1>
-      {movies.map(({ id, title, images: {thumbnail} }) => (
-        <div key={id}>
-          <div>
+    <Container>
+      {movies.map(({ id, title, images: { thumbnail } }) => (
+        <Card key={id}>
+          <ThumbnailPlaceholder>
             {title}
-          </div>
-          <button key={id} onClick={() => goToDetails(id)}>
-            <img src={thumbnail} alt=""/>
-          </button>
-        </div>
+          </ThumbnailPlaceholder>
+          <img src={thumbnail} alt={title} title={title} key={id} onClick={() => goToDetails(id)}/>
+        </Card>
       ))}
-    </div>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  justify-content: space-between;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fill,minmax(140px,1fr));
+`;
+
+const Card = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  height: 209px;
+  img {
+   display: block;
+  }
+`;
+
+const ThumbnailPlaceholder = styled.div`
+  background: linear-gradient(to bottom, #555 30%, #666 50%, #555 70%);
+  color: white;
+    
+  // place it behind the thumbnail
+  position: absolute;
+  z-index: -1;
+  width: 140px;
+  height: 100%;
+  
+  // centralizes text
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  
+  // animation
+  @keyframes backgroundAnimation {
+    0% {
+     background-position: 0 -209px;
+    }
+    100% {
+     background-position: 0 209px;
+    }
+  }
+  animation-duration: 1.5s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-name: backgroundAnimation;
+  animation-timing-function: linear;
+`;
 
 function mapStateToProps(state: State) {
   return {
